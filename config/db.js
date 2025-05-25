0843a504d7d284068ea59a5fc1de2ea527e5954e
Akanksha <akankshaanand2512@gmail.com>
2025-05-25 16:48:18 +0530

    Prepare for Railway deployment with DATABASE_URL support

--- config/db.js
+++ config/db.js
@@ -1,12 +1,26 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+let db;
+
+if (process.env.DATABASE_URL) {
+  const url = new URL(process.env.DATABASE_URL);
+
+  db = mysql.createConnection({
+    host: url.hostname,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.replace("/", ""),
+    port: url.port,
+  });
+} else {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "school",
+  });
+}
 
 db.connect((err) => {
   if (err) throw err;
