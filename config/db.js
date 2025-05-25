584eb0c39726203f6d431e9ddad01deb4e9c4bd9
Akanksha <akankshaanand2512@gmail.com>
2025-05-25 17:10:32 +0530

    Removed procfile

--- config/db.js
+++ config/db.js
@@ -1,26 +1,12 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-let db;
-
-if (process.env.DATABASE_URL) {
-  const url = new URL(process.env.DATABASE_URL);
-
-  db = mysql.createConnection({
-    host: url.hostname,
-    user: url.username,
-    password: url.password,
-    database: url.pathname.replace("/", ""),
-    port: url.port,
-  });
-} else {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "school",
-  });
-}
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
   if (err) throw err;
